f31ef744ceca4015363d0784c1fc727b0f48d0e4
Mark Conroy <mark@mark.ie>
2024-08-20 13:07:56 +0100

    allows showing items as list or grid

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,35 +5,50 @@ const fetchGithubData = async () => {
 
   app.innerHTML = `
     <h2 class="repositories-title">Repositories (${allRepos.length})</h2>
-    <form id="search-form" class="search-form">
-      <label for="search">Search by title or description</label>
-      <input type="text" id="search" name="search" placeholder="Search" />
-    </form>
-    <ul class="repos-list grid grid--thirds grid--gap-large">
-      ${allRepos
-        .map((repo) => {
-          return `
-        <li class="repos-list__item">
-          <article class="repo flow">
-            <h3><a href="${repo.html_url}">${repo.name}</a></h3>
-            ${repo.description ? `<p>${repo.description}</p>` : ""}
-            <ul class="repo__links flow">
-            <li><a href="${repo.html_url}/issues">Open Issues: ${repo.open_issues_count}</a></li>
-            <li><a href="${repo.html_url}/pulls">Open PRs: ${repo.open_issues_count}</a></li>
-            <li><a href="${repo.html_url}/commits">Latest Commits</a></li>
-            <li><a href="${repo.html_url}/releases">Releases</a></li>
-            <li>Clone command:<br> <code>git clone ${repo.ssh_url}</code></li>
-            </ul>
-          </article>
-        </li>
-      `;
-        })
-        .join("")}
-    </ul>
+    <div class="repositories flow flow--large">
+      <form id="search-form" class="search-form">
+        <label for="search">Search by title or description</label>
+        <input type="text" id="search" name="search" placeholder="Search" />
+      </form>
+
+      <div class="repositories__controls">
+        <h3>View as:</h3>
+        <div class="quick-links">
+          <ul>
+            <li><button class="button-link" data-active="true">Grid</button></li>
+            <li><button class="button-link" data-active="false">List</button></li>
+          </ul>
+        </div>
+      </div>
+
+      <ul class="repos-list repos-list--grid grid grid--thirds grid--gap-large">
+        ${allRepos
+          .map((repo) => {
+            return `
+          <li class="repos-list__item">
+            <article class="repo flow">
+              <h3><a href="${repo.html_url}">${repo.name}</a></h3>
+              ${repo.description ? `<p>${repo.description}</p>` : ""}
+              <ul class="repo__links flow">
+              <li><a href="${repo.html_url}/issues">Open Issues: ${repo.open_issues_count}</a></li>
+              <li><a href="${repo.html_url}/pulls">Open PRs</a></li>
+              <li><a href="${repo.html_url}/commits">Latest Commits</a></li>
+              <li><a href="${repo.html_url}/releases">Releases</a></li>
+              <li>Clone command:<br> <code>git clone ${repo.ssh_url}</code></li>
+              </ul>
+            </article>
+          </li>
+        `;
+          })
+          .join("")}
+      </ul>
+    </div>
   `;
+
   const repositoriesTitle = document.querySelector(".repositories-title");
   const searchForm = document.querySelector("#search-form");
   const searchInput = document.querySelector("#search");
+
   searchForm.addEventListener("keyup", (event) => {
     const searchValue = searchInput.value;
     const filteredRepos = allRepos.filter((repo) => {
@@ -53,6 +68,24 @@ const fetchGithubData = async () => {
     });
     repositoriesTitle.textContent = `Repositories (${filteredRepos.length})`;
   });
+
+  const quickLinks = document.querySelector(".repositories__controls .quick-links");
+  quickLinks.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+      const activeButton = quickLinks.querySelector("[data-active=true]");
+      activeButton.dataset.active = false;
+      event.target.dataset.active = true;
+      const reposList = document.querySelector(".repos-list");
+      // Remove all classes
+      reposList.className = "";
+      if (event.target.textContent === "Grid") {
+        reposList.classList.add("repos-list", "repos-list--grid", "grid", "grid--thirds", "grid--gap-large");
+      } else if (event.target.textContent === "List") {
+        reposList.classList.add("repos-list", "repos-list--list", "flow", "flow--large");
+      }
+    }
+  });
+
 };
 
 fetchGithubData();
